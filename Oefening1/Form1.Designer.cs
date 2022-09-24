1c523e4f0ee6375d3e4a7b9276cd388353c60032
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-09-24 15:59:58 +0200

    Add project files.

---
+++ Oefening1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Oefening1
+{
+    partial class Begroeting
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOne = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(338, 89);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(178, 89);
+            this.btnOne.TabIndex = 0;
+            this.btnOne.Text = "OK";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // Begroeting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOne);
+            this.Name = "Begroeting";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnOne;
+    }
+}
---
+++ Oefening1/Form1.cs
@@ -0,0 +1,15 @@
+namespace Oefening1
+{
+    public partial class Begroeting : Form
+    {
+        public Begroeting()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
