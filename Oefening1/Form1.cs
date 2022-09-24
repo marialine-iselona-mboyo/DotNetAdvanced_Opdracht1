112174f28d6044682ef7ef303891c41a0da153de
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-09-24 21:00:44 +0200

    Changes => Managed to separate de date and time, but have to change the user name and the language of the date&time

--- Oefening1/Form1.cs
+++ Oefening1/Form1.cs
@@ -10,12 +10,19 @@ namespace Oefening1
         private void btnOne_Click(object sender, EventArgs e)
         {
             string userName = Environment.UserName;
-            DateTime date = DateTime.Now;
-            //this.txtBox.Text = DateTime.Now.ToShortTimeString();
-            //this.txtBox.Text = DateTime.Now.ToShortTimeString();
 
-            MessageBox.Show(userName);           
-            MessageBox.Show(date.ToString());
+
+            //DateTime date = DateTime.Now;
+            //MessageBox.Show("Hello " + userName + ". Het is " + date.ToString() + " op ");
+
+            MessageBox.Show("Hello " + userName + ". Het is " + DateTime.Now.ToShortTimeString() + " op " + DateTime.Now.ToShortDateString());
+
+
+
+
+            //Vragen:
+            //Hoe Kan ik de username veranderen?
+            //Is mijn tijd en datum in de engelse formaat omdat mijn systeem in het engels is?
         }
     }
 }
