f43081aa788688792dfbfbd79251f84b3ffbaaff
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-09-26 00:19:50 +0200

    Changes => Tried the Do/While Loop

--- Oefening3/Form1.Designer.cs
+++ Oefening3/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(48, 52);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnThree.Size = new System.Drawing.Size(112, 34);
+            this.btnThree.TabIndex = 1;
+            this.btnThree.Text = "Click here!";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThree);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button btnThree;
     }
 }
--- Oefening3/Form1.cs
+++ Oefening3/Form1.cs
@@ -6,5 +6,21 @@ namespace Oefening3
         {
             InitializeComponent();
         }
+
+        private void btnThree_Click_1(object sender, EventArgs e)
+        {
+            int i = 0;
+            string s = "";
+
+            do
+            {
+                    s += (i * 7).ToString() + ", ";
+                    i++;
+                    //Console.WriteLine(i++);
+            }
+            while (i > 546);
+
+            MessageBox.Show(s);
+        }
     }
 }
