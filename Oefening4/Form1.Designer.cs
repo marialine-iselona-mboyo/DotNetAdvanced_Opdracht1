972841edc8736399a04496c0a030b4400b2cf1eb
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-09-26 22:45:07 +0200

    Starting oefening 4

--- Oefening4/Form1.Designer.cs
+++ Oefening4/Form1.Designer.cs
@@ -28,45 +28,376 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.calculate = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.shippingBox = new System.Windows.Forms.TextBox();
+            this.salesBox = new System.Windows.Forms.TextBox();
+            this.taxBox = new System.Windows.Forms.TextBox();
+            this.subtotalBox = new System.Windows.Forms.TextBox();
+            this.extPriceBox4 = new System.Windows.Forms.TextBox();
+            this.extPriceBox3 = new System.Windows.Forms.TextBox();
+            this.extPriceBox2 = new System.Windows.Forms.TextBox();
+            this.extPriceBox1 = new System.Windows.Forms.TextBox();
+            this.priceBox4 = new System.Windows.Forms.TextBox();
+            this.priceBox3 = new System.Windows.Forms.TextBox();
+            this.priceBox2 = new System.Windows.Forms.TextBox();
+            this.priceBox1 = new System.Windows.Forms.TextBox();
+            this.itemBox4 = new System.Windows.Forms.TextBox();
+            this.itemBox3 = new System.Windows.Forms.TextBox();
+            this.itemBox2 = new System.Windows.Forms.TextBox();
+            this.itemBox1 = new System.Windows.Forms.TextBox();
+            this.grandTotal = new System.Windows.Forms.Label();
+            this.shipping = new System.Windows.Forms.Label();
+            this.salesTax = new System.Windows.Forms.Label();
+            this.taxRate = new System.Windows.Forms.Label();
+            this.subtotal = new System.Windows.Forms.Label();
+            this.extPrice = new System.Windows.Forms.Label();
+            this.priceEach = new System.Windows.Forms.Label();
+            this.quantity = new System.Windows.Forms.Label();
+            this.item = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(704, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(360, 225);
-            this.dataGridView1.TabIndex = 0;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.calculate);
+            this.panel1.Controls.Add(this.numericUpDown4);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.totalBox);
+            this.panel1.Controls.Add(this.shippingBox);
+            this.panel1.Controls.Add(this.salesBox);
+            this.panel1.Controls.Add(this.taxBox);
+            this.panel1.Controls.Add(this.subtotalBox);
+            this.panel1.Controls.Add(this.extPriceBox4);
+            this.panel1.Controls.Add(this.extPriceBox3);
+            this.panel1.Controls.Add(this.extPriceBox2);
+            this.panel1.Controls.Add(this.extPriceBox1);
+            this.panel1.Controls.Add(this.priceBox4);
+            this.panel1.Controls.Add(this.priceBox3);
+            this.panel1.Controls.Add(this.priceBox2);
+            this.panel1.Controls.Add(this.priceBox1);
+            this.panel1.Controls.Add(this.itemBox4);
+            this.panel1.Controls.Add(this.itemBox3);
+            this.panel1.Controls.Add(this.itemBox2);
+            this.panel1.Controls.Add(this.itemBox1);
+            this.panel1.Controls.Add(this.grandTotal);
+            this.panel1.Controls.Add(this.shipping);
+            this.panel1.Controls.Add(this.salesTax);
+            this.panel1.Controls.Add(this.taxRate);
+            this.panel1.Controls.Add(this.subtotal);
+            this.panel1.Controls.Add(this.extPrice);
+            this.panel1.Controls.Add(this.priceEach);
+            this.panel1.Controls.Add(this.quantity);
+            this.panel1.Controls.Add(this.item);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(476, 597);
-            this.panel1.TabIndex = 1;
+            this.panel1.Size = new System.Drawing.Size(559, 569);
+            this.panel1.TabIndex = 0;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(66, 386);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(112, 34);
+            this.calculate.TabIndex = 31;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(191, 216);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(80, 31);
+            this.numericUpDown4.TabIndex = 30;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(191, 166);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(80, 31);
+            this.numericUpDown3.TabIndex = 29;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(191, 118);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 31);
+            this.numericUpDown1.TabIndex = 28;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(191, 67);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(80, 31);
+            this.numericUpDown2.TabIndex = 27;
+            // 
+            // totalBox
+            // 
+            this.totalBox.Location = new System.Drawing.Point(443, 509);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.Size = new System.Drawing.Size(90, 31);
+            this.totalBox.TabIndex = 25;
+            // 
+            // shippingBox
+            // 
+            this.shippingBox.Location = new System.Drawing.Point(443, 462);
+            this.shippingBox.Name = "shippingBox";
+            this.shippingBox.Size = new System.Drawing.Size(90, 31);
+            this.shippingBox.TabIndex = 24;
+            // 
+            // salesBox
+            // 
+            this.salesBox.Location = new System.Drawing.Point(443, 420);
+            this.salesBox.Name = "salesBox";
+            this.salesBox.Size = new System.Drawing.Size(90, 31);
+            this.salesBox.TabIndex = 23;
+            // 
+            // taxBox
+            // 
+            this.taxBox.Location = new System.Drawing.Point(443, 370);
+            this.taxBox.Name = "taxBox";
+            this.taxBox.Size = new System.Drawing.Size(90, 31);
+            this.taxBox.TabIndex = 22;
+            // 
+            // subtotalBox
+            // 
+            this.subtotalBox.Location = new System.Drawing.Point(443, 322);
+            this.subtotalBox.Name = "subtotalBox";
+            this.subtotalBox.Size = new System.Drawing.Size(90, 31);
+            this.subtotalBox.TabIndex = 21;
+            // 
+            // extPriceBox4
+            // 
+            this.extPriceBox4.Location = new System.Drawing.Point(443, 215);
+            this.extPriceBox4.Name = "extPriceBox4";
+            this.extPriceBox4.Size = new System.Drawing.Size(90, 31);
+            this.extPriceBox4.TabIndex = 20;
+            // 
+            // extPriceBox3
+            // 
+            this.extPriceBox3.Location = new System.Drawing.Point(443, 165);
+            this.extPriceBox3.Name = "extPriceBox3";
+            this.extPriceBox3.Size = new System.Drawing.Size(90, 31);
+            this.extPriceBox3.TabIndex = 19;
+            // 
+            // extPriceBox2
+            // 
+            this.extPriceBox2.Location = new System.Drawing.Point(443, 117);
+            this.extPriceBox2.Name = "extPriceBox2";
+            this.extPriceBox2.Size = new System.Drawing.Size(90, 31);
+            this.extPriceBox2.TabIndex = 18;
+            // 
+            // extPriceBox1
+            // 
+            this.extPriceBox1.Location = new System.Drawing.Point(443, 66);
+            this.extPriceBox1.Name = "extPriceBox1";
+            this.extPriceBox1.Size = new System.Drawing.Size(90, 31);
+            this.extPriceBox1.TabIndex = 17;
+            // 
+            // priceBox4
+            // 
+            this.priceBox4.Location = new System.Drawing.Point(315, 215);
+            this.priceBox4.Name = "priceBox4";
+            this.priceBox4.Size = new System.Drawing.Size(90, 31);
+            this.priceBox4.TabIndex = 16;
+            this.priceBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // priceBox3
+            // 
+            this.priceBox3.Location = new System.Drawing.Point(315, 165);
+            this.priceBox3.Name = "priceBox3";
+            this.priceBox3.Size = new System.Drawing.Size(90, 31);
+            this.priceBox3.TabIndex = 15;
+            this.priceBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // priceBox2
+            // 
+            this.priceBox2.Location = new System.Drawing.Point(315, 117);
+            this.priceBox2.Name = "priceBox2";
+            this.priceBox2.Size = new System.Drawing.Size(90, 31);
+            this.priceBox2.TabIndex = 14;
+            this.priceBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // priceBox1
+            // 
+            this.priceBox1.Location = new System.Drawing.Point(315, 66);
+            this.priceBox1.Name = "priceBox1";
+            this.priceBox1.Size = new System.Drawing.Size(90, 31);
+            this.priceBox1.TabIndex = 13;
+            this.priceBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // itemBox4
+            // 
+            this.itemBox4.Location = new System.Drawing.Point(26, 215);
+            this.itemBox4.Name = "itemBox4";
+            this.itemBox4.Size = new System.Drawing.Size(131, 31);
+            this.itemBox4.TabIndex = 12;
+            // 
+            // itemBox3
+            // 
+            this.itemBox3.Location = new System.Drawing.Point(26, 165);
+            this.itemBox3.Name = "itemBox3";
+            this.itemBox3.Size = new System.Drawing.Size(131, 31);
+            this.itemBox3.TabIndex = 11;
+            // 
+            // itemBox2
+            // 
+            this.itemBox2.Location = new System.Drawing.Point(26, 117);
+            this.itemBox2.Name = "itemBox2";
+            this.itemBox2.Size = new System.Drawing.Size(131, 31);
+            this.itemBox2.TabIndex = 10;
+            // 
+            // itemBox1
+            // 
+            this.itemBox1.Location = new System.Drawing.Point(26, 66);
+            this.itemBox1.Name = "itemBox1";
+            this.itemBox1.Size = new System.Drawing.Size(131, 31);
+            this.itemBox1.TabIndex = 9;
+            // 
+            // grandTotal
+            // 
+            this.grandTotal.AutoSize = true;
+            this.grandTotal.Location = new System.Drawing.Point(267, 509);
+            this.grandTotal.Name = "grandTotal";
+            this.grandTotal.Size = new System.Drawing.Size(102, 25);
+            this.grandTotal.TabIndex = 8;
+            this.grandTotal.Text = "Grand Total";
+            // 
+            // shipping
+            // 
+            this.shipping.AutoSize = true;
+            this.shipping.Location = new System.Drawing.Point(267, 465);
+            this.shipping.Name = "shipping";
+            this.shipping.Size = new System.Drawing.Size(83, 25);
+            this.shipping.TabIndex = 7;
+            this.shipping.Text = "Shipping";
+            // 
+            // salesTax
+            // 
+            this.salesTax.AutoSize = true;
+            this.salesTax.Location = new System.Drawing.Point(269, 423);
+            this.salesTax.Name = "salesTax";
+            this.salesTax.Size = new System.Drawing.Size(81, 25);
+            this.salesTax.TabIndex = 6;
+            this.salesTax.Text = "Sales Tax";
+            // 
+            // taxRate
+            // 
+            this.taxRate.AutoSize = true;
+            this.taxRate.Location = new System.Drawing.Point(267, 373);
+            this.taxRate.Name = "taxRate";
+            this.taxRate.Size = new System.Drawing.Size(76, 25);
+            this.taxRate.TabIndex = 5;
+            this.taxRate.Text = "Tax Rate";
+            // 
+            // subtotal
+            // 
+            this.subtotal.AutoSize = true;
+            this.subtotal.Location = new System.Drawing.Point(267, 325);
+            this.subtotal.Name = "subtotal";
+            this.subtotal.Size = new System.Drawing.Size(79, 25);
+            this.subtotal.TabIndex = 4;
+            this.subtotal.Text = "Subtotal";
+            // 
+            // extPrice
+            // 
+            this.extPrice.AutoSize = true;
+            this.extPrice.Location = new System.Drawing.Point(443, 38);
+            this.extPrice.Name = "extPrice";
+            this.extPrice.Size = new System.Drawing.Size(81, 25);
+            this.extPrice.TabIndex = 3;
+            this.extPrice.Text = "Ext. Price";
+            // 
+            // priceEach
+            // 
+            this.priceEach.AutoSize = true;
+            this.priceEach.Location = new System.Drawing.Point(315, 38);
+            this.priceEach.Name = "priceEach";
+            this.priceEach.Size = new System.Drawing.Size(90, 25);
+            this.priceEach.TabIndex = 2;
+            this.priceEach.Text = "Price Each";
+            // 
+            // quantity
+            // 
+            this.quantity.AutoSize = true;
+            this.quantity.Location = new System.Drawing.Point(191, 38);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(80, 25);
+            this.quantity.TabIndex = 1;
+            this.quantity.Text = "Quantity";
+            // 
+            // item
+            // 
+            this.item.AutoSize = true;
+            this.item.Location = new System.Drawing.Point(26, 38);
+            this.item.Name = "item";
+            this.item.Size = new System.Drawing.Size(48, 25);
+            this.item.TabIndex = 0;
+            this.item.Text = "Item";
             // 
             // SalesTaxCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1076, 749);
+            this.ClientSize = new System.Drawing.Size(581, 597);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "SalesTaxCalculator";
             this.Text = "SalesTaxCalculator";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
         private Panel panel1;
+        private Button calculate;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private TextBox totalBox;
+        private TextBox shippingBox;
+        private TextBox salesBox;
+        private TextBox taxBox;
+        private TextBox subtotalBox;
+        private TextBox extPriceBox4;
+        private TextBox extPriceBox3;
+        private TextBox extPriceBox2;
+        private TextBox extPriceBox1;
+        private TextBox priceBox4;
+        private TextBox priceBox3;
+        private TextBox priceBox2;
+        private TextBox priceBox1;
+        private TextBox itemBox4;
+        private TextBox itemBox3;
+        private TextBox itemBox2;
+        private TextBox itemBox1;
+        private Label grandTotal;
+        private Label shipping;
+        private Label salesTax;
+        private Label taxRate;
+        private Label subtotal;
+        private Label extPrice;
+        private Label priceEach;
+        private Label quantity;
+        private Label item;
     }
 }
--- Oefening4/Form1.cs
+++ Oefening4/Form1.cs
@@ -1,3 +1,22 @@
+/*
+ Maak het programma SalesTaxCalculator:
+
+Voorzie 4 lijnen met telkens de volgende velden die de gebruiker kan invullen:
+
+ Item (De naam van een item)
+ Quantity (De hoeveelheid van het item)
+ Price Each (De eenheidsprijs van het item)
+Voorzie de andere velden zoals op de afbeelding, waarbij Tax Rate ook door de gebruiker in te vullen is.
+
+Wanneer de gebruiker op de Calculate knop drukt:
+
+bereken en toon Ext. Price (quantity * price Each)
+bereken en toon Subtotal
+bereken en toon Sales Tax (subtotal * tax rate)
+bereken en toon Grand Total (subtotal + sales sax + shipping)
+ */
+
+
 namespace Oefening4
 {
     public partial class SalesTaxCalculator : Form
@@ -5,6 +24,11 @@ namespace Oefening4
         public SalesTaxCalculator()
         {
             InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
